d1d891eee669466bbc9ede08da4299861a78f9b8
Max <maxblanck@outlook.fr>
2020-12-11 11:47:27 +0100

    php Partie 3  Exercice 7

--- index.php
+++ index.php
@@ -11,12 +11,9 @@
     <h1>Exercice 7 Partie 3</h1>
 
     <ul>
-        <?php
-        for ($number = 1; $number <= 100; $number += 15)
-        {
-            echo '<li>' . 'On tient le bon bout' . '</li>';
-        }
-        ?></ul>
+        <?php for ($number = 1; $number <= 100; $number += 15): ?>
+        <li>On tient le bon bout</li>
+        <?php endfor; ?></ul>
 </body>
 
 </html>
